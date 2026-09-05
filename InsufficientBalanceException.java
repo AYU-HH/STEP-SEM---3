1cdc7be30e262a369253e1186096c0197f787912
Ayush Kumar <260607485+AYU-HH@users.noreply.github.com>
2026-09-05 14:08:46 +0530

    Remove comment from InsufficientBalanceException
    Removed unnecessary documentation comment.

--- InsufficientBalanceException.java
+++ InsufficientBalanceException.java
@@ -1,8 +1,4 @@
-/**
- * Thrown when attempting to withdraw more than available balance.
- */
 public class InsufficientBalanceException extends AccountException {
-
     public InsufficientBalanceException(String message) {
         super(message);
     }
