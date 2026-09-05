5d8276cad9f7fc1a97654c274e7bbeb6c61289de
Ayush Kumar <260607485+AYU-HH@users.noreply.github.com>
2026-09-05 14:08:25 +0530

    Update InactiveAccountException.java

--- InactiveAccountException.java
+++ InactiveAccountException.java
@@ -1,8 +1,4 @@
-/**
- * Thrown when operation is attempted on an inactive account.
- */
 public class InactiveAccountException extends AccountException {
-
     public InactiveAccountException(String message) {
         super(message);
     }
